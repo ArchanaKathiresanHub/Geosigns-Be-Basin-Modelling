e37f86aec83f419fb4887bc3b316a3b4228f86cb
Sergey Koshelev <sergey.koshelev@pds.nl>
2013-02-07 11:21:31 +0000

    New C like wrapper around PTDiagramCalculator function was added to EosPack DLL. New C# test was created to check the functionality of PTDiagramCalculator
    work-items: #14290
    git-tfs-id: [https://sede-pt-ce2.visualstudio.com/DefaultCollection]$/Basin Modeling/IBS/Trunk;C34505

--- development/libraries/EosPack/src/DllExport.h
+++ development/libraries/EosPack/src/DllExport.h
@@ -4,6 +4,7 @@
 #include "EosPack.h"
 #include "ComponentManager.h"
 #include "polynomials.h"
+#include "PTDiagCalculator.h"
 
 #include <vector>
 #include <string>
@@ -13,9 +14,7 @@ using std::string;
 
 namespace pvtFlash
 {
-
-
-   #if defined(_WIN32) || defined (_WIN64)	
+#if defined(_WIN32) || defined (_WIN64)   
 
 
    struct ComputeStruct
@@ -34,7 +33,7 @@ namespace pvtFlash
    extern "C" 
    {
       EOSPACK_DLL_EXPORT void __stdcall SetPvtPropertiesConfigFile(char* fileName)
-      {	
+      {  
          pvtFlash::pvtPropertiesConfigFile = string(fileName);
       }
       
@@ -43,24 +42,28 @@ namespace pvtFlash
          double phaseCompMasses[N_PHASES][CBMGenerics::ComponentManager::NumberOfOutputSpecies];
          pvtFlash::EosPack& instance = pvtFlash::EosPack::getInstance();
          
-         bool result = instance.computeWithLumping(
-                                                   computeInfo->temperature, 
-                                                   computeInfo->pressure, 
-                                                   computeInfo->compMasses,
-                                                   phaseCompMasses,
-                                                   computeInfo->phaseDensity, 
-                                                   computeInfo->phaseViscosity, 
-                                                   computeInfo->isGormPrescribed, 
-                                                   computeInfo->gorm);
+         bool result = instance.computeWithLumping( computeInfo->temperature, 
+                                                    computeInfo->pressure, 
+                                                    computeInfo->compMasses,
+                                                    phaseCompMasses,
+                                                    computeInfo->phaseDensity, 
+                                                    computeInfo->phaseViscosity, 
+                                                    computeInfo->isGormPrescribed, 
+                                                    computeInfo->gorm
+                                                  );
          
          int index = 0;
          for( int i = 0; i < N_PHASES; i++ )
+         {
             for( int j = 0; j < CBMGenerics::ComponentManager::NumberOfOutputSpecies; j++)
+            {
                computeInfo->phaseCompMasses[index++] = phaseCompMasses[i][j];
+            }
+         }
          return result;
       }
       
-	  EOSPACK_DLL_EXPORT double __stdcall GetMolWeight(int componentId, double gorm)
+      EOSPACK_DLL_EXPORT double __stdcall GetMolWeight(int componentId, double gorm)
       {
          pvtFlash::EosPack& instance = pvtFlash::EosPack::getInstance();
          return instance.getMolWeightLumped(componentId, gorm);
@@ -70,9 +73,92 @@ namespace pvtFlash
       {
          pvtFlash::EosPack& instance = pvtFlash::EosPack::getInstance();
          computeInfo->gorm = instance.gorm(computeInfo->compMasses);
-		 return computeInfo->gorm;
+         return computeInfo->gorm;
       }
-      
+
+      /// \brief Create PT phase diagram
+      /// \param diagType - type of diagram, 0 - mass, 1 - mole, 2 - volume
+      /// \param T trap temperature
+      /// \param P trap pressure
+      /// \param[in] comp array of size 23 for composition mass fractions
+      /// \param[out] points array, size of 8 which will contain on return CriticalT, CriticalP, BubbleT, BubbleP, CricondenthermT, CricondenthermP, CricondenbarT, CricondenbarP
+      /// \param[in,out] szIso array sizeof 11 (number of isolines) for each isoline it contains on input maximum number of isoline points allocated in isoline array,
+      ///                      on output it contains real number of points for each isoline
+      /// \param[out] isolines 1D array which keeps T,P values for each isoline, number of points for each isoline keeps szIso array 
+      /// \return true on success, false otherwise
+      EOSPACK_DLL_EXPORT bool __stdcall BuildPTDiagram( int diagType, double T, double P, double * comp, double * points, int * szIso, double * isolines )
+      {
+         const int iNc = CBMGenerics::ComponentManager::NumberOfOutputSpecies;
+
+         std::vector<double> masses( iNc );
+         std::copy( comp, comp + iNc, masses.begin() );
+        
+         PTDiagramCalculator::DiagramType dType = PTDiagramCalculator::MoleMassFractionDiagram;
+         switch( diagType )
+         {
+            case 0:  dType = PTDiagramCalculator::MassFractionDiagram;     break;
+            case 1:  dType = PTDiagramCalculator::MoleMassFractionDiagram; break;
+            case 2:  dType = PTDiagramCalculator::VolumeFractionDiagram;   break;
+            default: break; // the default is MoleMassFraction diagram
+         }
+
+         try
+         {
+            PTDiagramCalculator diagBuilder( dType, masses );
+
+            diagBuilder.setAoverBTerm( 2 );
+            diagBuilder.setNonLinSolverConvPrms( 1e-6, 500 );
+
+            diagBuilder.findBubbleDewLines( T, P, std::vector<double>() );
+
+            // fill critical point values
+            const std::pair<double,double> & critPt = diagBuilder.getCriticalPoint();
+            points[0] = critPt.first;
+            points[1] = critPt.second;
+
+            // fill bubble point values
+            points[2] = T;
+            double bubbleP;
+            if ( diagBuilder.getBubblePressure( T, bubbleP ) )
+            {
+               points[3] = bubbleP;
+            }
+            else
+            {
+               points[3] = P;
+            }
+            // fill cricondentherm point values
+            const std::pair<double, double> & tPt = diagBuilder.getCricondenthermPoint();
+            points[4] = tPt.first;
+            points[5] = tPt.second;
+
+            // fill cricondenbar point values
+            const std::pair<double, double> & pPt = diagBuilder.getCricondenbarPoint();
+            points[6] = pPt.first;
+            points[7] = pPt.second;
+
+            // fill isolines array for 11 isoline, first - dew line, last bubble line
+            double vals[]   = { 0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0 };
+            std::vector<double> isoData;
+            size_t is = 0;
+            for ( int i = 0; i < sizeof(vals)/sizeof(double); ++i )
+            {
+               const std::vector< std::pair<double,double> > & isoline = diagBuilder.calcContourLine( vals[i] );
+               szIso[i] = std::min( static_cast<int>(isoline.size()), szIso[i] );
+               for ( size_t it = 0; it < szIso[i]; ++it )
+               {
+                  isolines[is]   = isoline[it].first;
+                  isolines[is+1] = isoline[it].second;
+                  is += 2;
+               }
+            }
+         }
+         catch( ... )
+         {
+            return false;
+         }
+         return true;
+      }
    }
 #endif
 
---
+++ development/libraries/EosPack/test/PTDiagCalculatorTest.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Runtime.InteropServices;
+
+namespace PTDiagCalculator.Test
+{
+    
+    
+    /// <summary>
+    ///This is a test class for PTDiagramCalculator
+    ///</summary>
+    [TestClass()]
+    public class PTDiagCalculatorTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+        [DllImport("EosPack.dll", EntryPoint = "SetPvtPropertiesConfigFile")]
+        private static extern void EosPackSetPvtPropertiesConfigFile(string fileName);
+
+        [DllImport("EosPack.dll", EntryPoint = "BuildPTDiagram")]
+        private static extern bool BuildPTDiagram( int diagType, double T, double P, [In] double[] comp, [Out] double[] points, 
+                                                   [In, Out] int[] szIso, [Out] double[] isolines );
+
+        [TestMethod]
+        public void TestPTDiagramCalculator()
+        {
+           string pvtPropertiesConfigFile = "..\\..\\..\\csharp-test\\PTDiagCalculatorTest.cfg";
+           EosPackSetPvtPropertiesConfigFile(pvtPropertiesConfigFile);
+           // define composition
+           double[] comp = new double[] { 4.63937e+10, 3.76229e+11, 7.53617e+10, 1.60934e+11, 7.01346e+10, 1.93474e+11, 6.20802e+09, 6.26855e+09, 6.3693e+09, 
+                                          7.4708e+09, 7.78099e+09, 0, 2.65935e+09, 5.4181e+08, 8.16853e+09, 2.68247e+10, 4.82603e+09, 5.65998e+07, 2.17633e+07,
+                                          0, 0, 0, 0 };
+           // allocate space for special points
+           double[] points = new double[8]; // 4 points - Critical point, Bubble point, Cricondentherm point, Cricondenbar point
+           // allocate space for isolines. It will be 11 isolines 0 : 0.1 : 1, up to 400 points (2 double per point) per isoline
+           int[] szIso = new int[] { 400, 400, 400, 400, 400, 400, 400, 400, 400, 400, 400 };
+           double[] isolines = new double[11 * 400 * 2];
+           // Call diagram builder
+           bool ret = BuildPTDiagram(0, 588.15, 12665600, comp, points, szIso, isolines);
+           // check results
+           Assert.IsTrue( ret, "PTDiagramBuilder failed to create diagram for unknown reason" );
+           
+           int[] checkSz = new int[] { 65, 55, 55, 54, 57, 60, 69, 77, 87, 93, 125 };
+           for ( int i = 0; i < 11; ++i )
+           {
+              Assert.AreEqual( szIso[i], checkSz[i] );
+           }
+           Assert.IsTrue(Math.Abs(points[0] - 909.194      ) < 1e-3, "Wrong critical point temperature value " + Convert.ToString(points[0]));
+           Assert.IsTrue(Math.Abs(points[1] - 11080924.440 ) < 1e-3, "Wrong critical point pressure value "    + Convert.ToString(points[1]));
+           Assert.IsTrue(Math.Abs(points[2] - 588.15       ) < 1e-2, "Wrong bubble point temperature value "   + Convert.ToString(points[2]));
+           Assert.IsTrue(Math.Abs(points[3] - 10057971.025 ) < 1e-3, "Wrong bubble point pressure value "      + Convert.ToString(points[3]));
+      }
+    }
+}
