7442da23d61fba27d7005eac1505612dcfe2404d
Alfred van der Hoeven <alfred.vanderhoeven@pds.nl>
2013-04-29 08:58:57 +0000

    Added bin directories for RHEL 6.3
    work-items: #16478
    git-tfs-id: [https://sede-pt-ce2.visualstudio.com/DefaultCollection]$/Basin Modeling/IBS/Trunk;C35723

--- development/InstallAll.csh.cmake
+++ development/InstallAll.csh.cmake
@@ -36,7 +36,7 @@ echo "Creating system directories"
 mkdir -p $mainSystemDirectory
 
 # system directory aliases
-set otherSystemDirectories = "Linux LinuxRHEL64_53AS LinuxRHEL64_53WS LinuxRHEL64_56AS LinuxRHEL64_56WS LinuxRHEL64_57AS LinuxRHEL64_57WS LinuxRHEL64_58AS LinuxRHEL64_58WS"
+set otherSystemDirectories = "Linux LinuxRHEL64_53AS LinuxRHEL64_53WS LinuxRHEL64_56AS LinuxRHEL64_56WS LinuxRHEL64_57AS LinuxRHEL64_57WS LinuxRHEL64_58AS LinuxRHEL64_58WS LinuxRHEL64_63AS LinuxRHEL64_63WS"
 echo "pwd = `pwd`"
 foreach otherSystemDirectory ($otherSystemDirectories)
    ln -s $mainSystemDirectory $otherSystemDirectory
