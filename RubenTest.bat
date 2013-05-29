35084785bc918b12dca15b776b120a3f9cbfd944
ruben.nijp@pds.nl <ruben.nijp@pds.nl>
2013-05-29 12:41:38 +0000

    .
    git-tfs-force:.
    git-tfs-id: [https://sede-pt-ce2.visualstudio.com/DefaultCollection]$/Basin Modeling/IBS/Trunk;C36475

--- RubenTest.bat
+++ RubenTest.bat
@@ -1,7 +1,3 @@
 
-cd "C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE"
-
-tf checkout $/Basin Modeling/IBS/Trunk/RubenTest
-tf undo RubenTest.bat /noprompt
-
-pause
+"C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\tf.exe" checkout $/Basin Modeling/IBS/Trunk/RubenTest
+"C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\tf.exe" undo RubenTest.bat /noprompt
