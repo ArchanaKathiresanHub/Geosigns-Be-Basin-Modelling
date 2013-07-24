a009a6317ba1d254d7ae50dbcb6ad70842aeed8a
William Senior <william.senior@pds.nl>
2013-07-24 12:32:50 +0000

    Updated unit test to include test for k-values.
    work-items: #18139
    git-tfs-id: [https://sede-pt-ce2.visualstudio.com/DefaultCollection]$/Basin Modeling/IBS/Trunk;C37830

--- development/libraries/EosPack/test/EosPackTest.C
+++ development/libraries/EosPack/test/EosPackTest.C
@@ -15,6 +15,88 @@ char * CfgFile   = "./PVT_properties.cfg";
 static void CreatePVTpropertiesCfgFile();
 static void InitializeCompositionMasses( double masses[] );
 
+
+static void test_InitialisationOfKValues () {
+
+
+
+
+   double pressure    = 39958145.0;  // in Pa
+   double temperature = 353.50111;   // in K
+   double compMasses[CBMGenerics::ComponentManager::NumberOfOutputSpecies];
+
+   compMasses [ ASPHALTENES ] = 5.9322774e-05;
+   compMasses [      RESINS ] = 0.00010834321;
+   compMasses [     C15_ARO ] = 0.0046930211;
+   compMasses [     C15_SAT ] = 0.015339124;
+   compMasses [    C6_14ARO ] = 0.053148382;
+   compMasses [    C6_14SAT ] = 0.073486113;
+   compMasses [          C5 ] = 0.016210614;
+   compMasses [          C4 ] = 0.02930958;
+   compMasses [          C3 ] = 0.044801003;
+   compMasses [          C2 ] = 0.066723203;
+   compMasses [          C1 ] = 0.67665664;
+   compMasses [         COX ] = 0;
+   compMasses [          N2 ] = 0.019464658;
+   compMasses [         H2S ] = 0;
+   compMasses [         LSC ] = 0;
+   compMasses [      C15_AT ] = 0;
+   compMasses [     C6_14BT ] = 0;
+   compMasses [    C6_14DBT ] = 0;
+   compMasses [     C6_14BP ] = 0;
+   compMasses [    C15_AROS ] = 0;
+   compMasses [    C15_SATS ] = 0;
+   compMasses [   C6_14SATS ] = 0;
+   compMasses [   C6_14AROS ] = 0;
+
+
+   double phaseMasses[CBMGenerics::ComponentManager::NumberOfPhases][CBMGenerics::ComponentManager::NumberOfOutputSpecies];
+
+   double phaseDensity[CBMGenerics::ComponentManager::NumberOfPhases];
+   double phaseViscosity[CBMGenerics::ComponentManager::NumberOfPhases];
+
+   double kValuesComputed  [ CBMGenerics::ComponentManager::NumberOfSpeciesToFlash ];
+   double kValuesExpected [ CBMGenerics::ComponentManager::NumberOfSpeciesToFlash ];
+   double gorm = 0.0; // Initialised to stop the compiler from complaining.
+                      // This is okay because isGormPrescribed is defined false, any gorm value will not be used.
+   bool isGormPrescribed = false;
+
+   // Indicate to EosPack that saving of k-values is required,
+   kValuesComputed [ 0 ] = -1.0;
+
+   pvtFlash::EosPack& instance = pvtFlash::EosPack::getInstance();
+         
+   bool result = instance.computeWithLumping ( temperature, 
+                                               pressure, 
+                                               compMasses,
+                                               phaseMasses,
+                                               phaseDensity, 
+                                               phaseViscosity, 
+                                               isGormPrescribed, 
+                                               gorm,
+                                               kValuesComputed );
+    
+   kValuesExpected [  0 ] = 6.60273155042658345e+03;
+   kValuesExpected [  1 ] = 1.30765460986769649e+04;
+   kValuesExpected [  2 ] = 1.73153010603771850e+03;
+   kValuesExpected [  3 ] = 3.73738426515636775e+01;
+   kValuesExpected [  4 ] = 5.20409170312865932e+00;
+   kValuesExpected [  5 ] = 2.59238570551743397e+00;
+   kValuesExpected [  6 ] = 1.99224092565726085e+00;
+   kValuesExpected [  7 ] = 1.59796701407822672e+00;
+   kValuesExpected [  8 ] = 1.21088348479272834e+00;
+   kValuesExpected [  9 ] = 9.07181261374219283e-01;
+   kValuesExpected [ 10 ] = 5.21685329290231370e-01;
+   kValuesExpected [ 11 ] = 9.94000165289730897e-01;
+   kValuesExpected [ 12 ] = 3.24734491437953743e-01;
+   kValuesExpected [ 13 ] = 1.53531376868468827e+00;
+
+   // The kvalues computed should be equal to those in the expected array.
+   for ( int i = 0; i < CBMGenerics::ComponentManager::NumberOfSpeciesToFlash; ++i ) {
+      assert ( kValuesComputed [ i ] == kValuesExpected [ i ] );
+   }
+
+}
       
 static void FlashVapourTest()
 {
@@ -217,6 +299,7 @@ int main(int argc, char ** argv)
    if (      !std::strcmp( argv[1], "cwlmp_capi" ) ) { test_ComputeWithLumping_CAPI(); }
    else if ( !std::strcmp( argv[1], "gorm_capi"  ) ) { test_GormCalculationTest();     }
    else if ( !std::strcmp( argv[1], "gmw_capi"   ) ) { test_GetMolWeight();            }
+   else if ( !std::strcmp( argv[1], "kvalues"    ) ) { test_InitialisationOfKValues(); }
    else
    {
       std::cerr << "Unknown test" << std::endl;
