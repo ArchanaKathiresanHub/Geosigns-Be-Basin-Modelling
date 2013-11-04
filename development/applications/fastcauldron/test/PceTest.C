f0b6497000b02b92d46a7807e4a4b658d92faef5
William Senior <william.senior@pds.nl>
2013-11-04 13:34:51 +0000

    Added unit test for capillary entry pressure.
    work-items: #21206, #21830
    git-tfs-id: [https://sede-pt-ce2.visualstudio.com/DefaultCollection]$/Basin Modeling/IBS/Trunk;C40007

---
+++ development/applications/fastcauldron/test/PceTest.C
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <cmath>
+#include <cassert>
+#include <cstdlib>
+
+#include "BrooksCorey.h"
+
+static const double DefaultC1 = 0.33;
+static const double DefaultC2 = 0.2611;
+
+bool isEqual ( const double x, const double y, const double tolerance = 1.0e-10 );
+
+
+int main () {
+
+   double permeability;
+   double pce;
+
+   permeability = 1.0e-8;
+   pce = BrooksCorey::computeCapillaryEntryPressure ( permeability, DefaultC1, DefaultC2 );
+   assert ( isEqual ( pce, 239276473.852775 ));
+
+   permeability = 1.0e-6;
+   pce = BrooksCorey::computeCapillaryEntryPressure ( permeability, DefaultC1, DefaultC2 );
+   assert ( isEqual ( pce, 52347988.7009069 ));
+
+   permeability = 1.0e-4;
+   pce = BrooksCorey::computeCapillaryEntryPressure ( permeability, DefaultC1, DefaultC2 );
+   assert ( isEqual ( pce, 11452492.0770789 ));
+
+   permeability = 1.0e-2;
+   pce = BrooksCorey::computeCapillaryEntryPressure ( permeability, DefaultC1, DefaultC2 );
+   assert ( isEqual ( pce, 2505532.26648195 ));
+
+   permeability = 1.0;
+   pce = BrooksCorey::computeCapillaryEntryPressure ( permeability, DefaultC1, DefaultC2 );
+   assert ( isEqual ( pce, 548150.734017655 ));
+
+   permeability = 1.0e2;
+   pce = BrooksCorey::computeCapillaryEntryPressure ( permeability, DefaultC1, DefaultC2 );
+   assert ( isEqual ( pce, 119922.314002360 ));
+
+   return 0;
+}
+
+
+
+bool isEqual ( const double x, const double y, const double tolerance ) {
+
+   if ( x == y ) {
+      return true;
+   } else {
+
+      double absX = std::abs ( x );
+      double absY = std::abs ( y );
+
+      if ( absX > absY ) {
+         return std::abs ( x - y ) <= tolerance * absX;
+      } else {
+         return std::abs ( x - y ) <= tolerance * absY;
+      }
+
+   }
+
+}
