753e04d7cabcc35e357bc7599bdc86c1b6d48803
w.suijlen <w.suijlen@pds.tfs.local>
2014-03-13 16:13:34 +0000

    "Added possibility to configure variables in script via environment"
    work-items: #14844, #25499
    git-tfs-id: [https://sede-pt-ce2.visualstudio.com/DefaultCollection]$/Basin Modeling/IBS/Trunk;C42269

--- development/BuildProjects/Linux-GetRegressionTestBench.sh
+++ development/BuildProjects/Linux-GetRegressionTestBench.sh
@@ -1,28 +1,53 @@
 #!/bin/bash
 
-# setting variables
-export PATH=/nfs/rvl/groups/ept-sg/SWEast/Cauldron/Tools/TEE-CLC/TEE-CLC-11.0.0:$PATH
+######################################################
+## Set variables                                   ###
+######################################################
 
-WORKSPACE=RegressionTestBench
-COLLECTION=https://tfs.sede-coe.pds.nl/tfs/COE-II
-ROOTDIRECTORY=$HOME/RegressionTestBench
+### Set the PATH environment variable
+tfsTools=/nfs/rvl/groups/ept-sg/SWEast/Cauldron/Tools/TEE-CLC/TEE-CLC-11.0.0
+export PATH=$tfsTools:/usr/bin/X11:/usr/kerberos/bin:/usr/local/bin:/bin:/usr/bin 
+
+### Set configurable environment variables
+if [ x$SCRIPT_WORKSPACE = x ]; then
+   SCRIPT_WORKSPACE="RegressionTestBench_${RANDOM}"
+fi
+
+if [ x$COLLECTION = x ]; then
+   COLLECTION=https://tfs.sede-coe.pds.nl/tfs/COE-II
+fi
+
+if [ x$SCRIPT_DIRECTORY = x ]; then
+   SCRIPT_DIRECTORY=$HOME/RegressionTestBench
+fi
+
+if [ x$SCRIPT_TFSFOLDER = x ]; then
+   SCRIPT_TFSFOLDER='$/Basin Modeling/IBS/Tests/RegressionTestBench'
+fi
+
+######################################################
+## Set-up script directory                         ###
+######################################################
 
 # Creating and mapping the workspace
-if tf workspaces -collection:${COLLECTION} -computer:* | grep -q ^${WORKSPACE} ; then
-    echo "- deleting existing workspace $WORKSPACE"
-    tf workspace -noprompt -delete -collection:$COLLECTION $WORKSPACE
+if tf workspaces -collection:${COLLECTION} | grep -q ^${SCRIPT_WORKSPACE} ; then
+    echo "- Deleting existing workspace $SCRIPT_WORKSPACE"
+    tf workspace -noprompt -delete -collection:$COLLECTION $SCRIPT_WORKSPACE
 fi
-echo "- Creating new workspace $WORKSPACE"
-tf workspace -noprompt -new -collection:$COLLECTION $WORKSPACE -comment:"RegressionTestBench" -location:local
-echo "- Mapping workspace $WORKSPACE to folder $ROOTDIRECTORY"
-tf workfold  -noprompt -map -collection:$COLLECTION -workspace:$WORKSPACE \$/Basin\ Modeling/IBS/Tests/RegressionTestBench $ROOTDIRECTORY
+echo "- Creating new workspace $SCRIPT_WORKSPACE"
+tf workspace -noprompt -new -collection:$COLLECTION $SCRIPT_WORKSPACE -comment:"RegressionTestBench" -location:local
+echo "- Mapping tfs folder $SCRIPT_TFSFOLDER in workspace $SCRIPT_WORKSPACE to folder $SCRIPT_DIRECTORY"
+tf workfold  -noprompt -map -collection:$COLLECTION -workspace:$SCRIPT_WORKSPACE "$SCRIPT_TFSFOLDER" $SCRIPT_DIRECTORY
 
 # creating directory to hold the scripts
-echo "- (Re-)creating work folder $ROOTDIRECTORY"
-rm -rf $ROOTDIRECTORY
-mkdir -p $ROOTDIRECTORY
-cd $ROOTDIRECTORY
+echo "- (Re-)creating work folder $SCRIPT_DIRECTORY"
+rm -rf $SCRIPT_DIRECTORY
+mkdir -p $SCRIPT_DIRECTORY
+cd $SCRIPT_DIRECTORY
 
 echo "`pwd` - Retrieving regression test bench ..."
-tf get -recursive -force "$ROOTDIRECTORY"
+tf get -recursive -force "$SCRIPT_DIRECTORY"
+
+echo "- Deleting workspace $SCRIPT_WORKSPACE"
+tf workspace -noprompt -delete -collection:$COLLECTION $SCRIPT_WORKSPACE
 
