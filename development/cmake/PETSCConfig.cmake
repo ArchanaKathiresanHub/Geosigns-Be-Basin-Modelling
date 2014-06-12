4fb5bc53932a5d22ce5f84e4edf5f5468c3dd4dc
w.suijlen <w.suijlen@pds.tfs.local>
2014-06-12 16:12:46 +0000

    Use the default find_library idiom instead.
    work-items: #26840, #29092
    git-tfs-id: [https://sede-pt-ce2.visualstudio.com/DefaultCollection]$/Basin Modeling/IBS/Trunk;C43798

--- development/cmake/PETSCConfig.cmake
+++ development/cmake/PETSCConfig.cmake
@@ -24,7 +24,7 @@ endif()
 set(PETSC_FOUND TRUE)
 set(PETSC_ROOT "${PETSC_HOME}" CACHE PATH "Installation directory of PETSc")
 set(PETSC_INCLUDE_DIRS "${PETSC_ROOT}/include")
-set(PETSC_LIBRARIES "${PETSC_ROOT}/lib/libpetsc.a")
+find_library(PETSC_LIBRARIES "petsc" PATHS "${PETSC_ROOT}/lib" NO_DEFAULT_PATH )
 set(PETSC_LINK_FLAGS "${OpenMP_CXX_FLAGS}")
 
 add_external_package_info( 
