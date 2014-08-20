0c7884b11eb66980551c23e57cc379e6cc62388b
w.suijlen <w.suijlen@pds.tfs.local>
2014-08-20 13:52:58 +0000

    Added a wrapper script around bsub that loads the LSF tools into the environment to run the given command on the cluster.
    work-items: #29945, #29965
    git-tfs-id: [https://sede-pt-ce2.visualstudio.com/DefaultCollection]$/Basin Modeling/IBS/Trunk;C45008

---
+++ development/BuildProjects/Linux-DoOnCluster.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+LSF_PROJECT=cldrn
+
+# Source LSF command line tools
+source /glb/apps/hpc/lsfprod/conf/profile.lsf
+
+# Submit command to cluster
+bsub -P $LSF_PROJECT -Is -q default.q "$@"
+
+# Print that we ended the job
+echo "<<Exited from Job>>"
