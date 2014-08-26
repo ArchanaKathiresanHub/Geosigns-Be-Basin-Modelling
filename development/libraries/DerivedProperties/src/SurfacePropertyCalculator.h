d4661eee333c043db636c3888015cfcaf7c48c3c
William Senior <william.senior@pds.nl>
2014-08-26 14:50:01 +0000

    First time under version control for the surface-property calculator class.
    work-items: #31459
    git-tfs-id: [https://sede-pt-ce2.visualstudio.com/DefaultCollection]$/Basin Modeling/IBS/Trunk;C45147

---
+++ development/libraries/DerivedProperties/src/SurfacePropertyCalculator.h
@@ -0,0 +1,39 @@
+#ifndef DERIVED_PROPERTIES__SURFACE_PROPERTY_CALCULATOR_H
+#define DERIVED_PROPERTIES__SURFACE_PROPERTY_CALCULATOR_H
+
+#include <boost/shared_ptr.hpp>
+
+#include "AbstractProperty.h"
+#include "AbstractSnapshot.h"
+#include "AbstractSurface.h"
+
+#include "SurfaceProperty.h"
+
+namespace DerivedProperties {
+
+   // Forward declaration of PropertyManager
+   class DerivedPropertyManager;
+
+   /// \brief Calculates a derived property.
+   class SurfacePropertyCalculator {
+
+   public :
+
+      virtual ~SurfacePropertyCalculator () {}
+
+      virtual void calculate ( PropertyManager& propManager,
+                               const DataModel::AbstractProperty* property,
+                               const DataModel::AbstractSnapshot* snapshot,
+                               const DataModel::AbstractSurface*  surface,
+                                     SurfacePropertyList&         derivedProperties ) const = 0;
+
+   };
+
+
+   typedef boost::shared_ptr<const SurfacePropertyCalculator> SurfacePropertyCalculatorPtr;
+
+
+} // namespace DerivedProperties {
+
+
+#endif // DERIVED_PROPERTIES__SURFACE_PROPERTY_CALCULATOR_H
