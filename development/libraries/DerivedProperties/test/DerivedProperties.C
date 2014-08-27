df0ef2d25f848856d613ed9bd4a235afb84cf128
Natalya Kuznetsova <natalya.kuznetsova@pds.nl>
2014-08-27 08:08:41 +0000

    Added unit test for DerivedProperties.
    work-items: #31458
    git-tfs-id: [https://sede-pt-ce2.visualstudio.com/DefaultCollection]$/Basin Modeling/IBS/Trunk;C45165

--- development/libraries/DerivedProperties/test/DerivedProperties.C
+++ development/libraries/DerivedProperties/test/DerivedProperties.C
@@ -1,4 +1,12 @@
 #include "../src/DerivedSurfaceProperty.h"
+#include "../src/DerivedFormationProperty.h"
+#include "../src/DerivedFormationSurfaceProperty.h"
+#include "MockProperty.h"
+#include "MockSnapshot.h"
+#include "MockFormation.h"
+#include "MockSurface.h"
+#include "MockGrid.h"
+
 #include <gtest/gtest.h>
 
 #include <iostream>
@@ -16,108 +24,49 @@ namespace
    static const int LastJ       = 4;
    static const int LastGhostI  = 6;
    static const int LastGhostJ  = 6;
+   static const unsigned int NumberK = 10;
 
-   static const std::string SurfaceName  = "Surface Test";
-   static const std::string PropertyName = "Property Test";
+   static const std::string SurfaceName   = "Surface Name";
+   static const std::string FormationName = "Formation Name";
+   static const std::string PropertyName  = "Property Name";
    static const double SnapshotAge  = 10.0;
 }
 
-class TestAbstractGrid : public AbstractGrid {
+TEST( DerivedSurfaceProperty, Test2 ) 
+{
+   MockGrid testGrid( FirstI, FirstGhostI, FirstJ, FirstGhostJ, LastI, LastGhostI, LastJ, LastGhostJ );
 
-public:
-
-   int firstI (bool withGhosts) const {
-
-      if ( withGhosts ) {
-         return FirstGhostI;
-      } else {
-         return FirstI;
-      }
-
-   }
+   EXPECT_EQ( FirstI, testGrid.firstI( false ));
+   EXPECT_EQ( FirstGhostI, testGrid.firstI( true ));
+   EXPECT_EQ( FirstJ, testGrid.firstJ( false ));
+   EXPECT_EQ( FirstGhostJ, testGrid.firstJ( true ));
+}
    
-   /// return the first local vertical index
-   int firstJ (bool withGhosts) const { 
 
-      if ( withGhosts ) {
-         return FirstGhostJ;
-      } else {
-         return FirstJ;
-      }
-
-   }
+TEST( DerivedSurfaceProperty, Test1 ) 
+{
+   MockGrid      testGrid( FirstI, FirstGhostI, FirstJ, FirstGhostJ, LastI, LastGhostI, LastJ, LastGhostJ );
+   MockSurface   testSurface( SurfaceName );
+   MockSnapshot  testSnapshot( SnapshotAge );
+   MockProperty  testProperty( PropertyName );
+   MockFormation testFormation( FormationName );
    
-   /// return the last local horizontal index
-   int lastI (bool withGhosts) const { 
+   DerivedProperties::DerivedSurfaceProperty s ( &testProperty, &testSnapshot, &testSurface, &testGrid );
+   DerivedProperties::DerivedFormationProperty f ( &testProperty, &testSnapshot, &testFormation, &testGrid, NumberK );
+   DerivedProperties::DerivedFormationSurfaceProperty sf ( &testProperty, &testSnapshot, &testFormation, &testSurface, &testGrid );
 
-      if ( withGhosts ) {
-         return LastGhostI;
-      } else {
-         return LastI;
-      }
-
-   }
-   
-   /// return the last local vertical index
-   int lastJ (bool withGhosts) const { 
-
-      if ( withGhosts ) {
-         return LastGhostJ;
-      } else {
-         return LastJ;
-      }
-
-   }
-   
-};
-
-class TestAbstractSnapshot : public AbstractSnapshot {
-
-public:
-   double getTime () const { return SnapshotAge; };
-};
-
-class TestAbstractSurface : public AbstractSurface {
-
-public:
-   const std::string& getName () const { return SurfaceName; };
-};
-
-class TestAbstractProperty : public AbstractProperty {
-
-public:
-   const std::string& getName () const { return PropertyName; };
-};
-
-class TestSurfaceProperty : public DerivedProperties::DerivedSurfaceProperty {
-
-public:
-   TestSurfaceProperty ( const TestAbstractProperty* prop,
-                         const TestAbstractSnapshot* ss,
-                         const TestAbstractSurface*  surf,
-                         const TestAbstractGrid*     g ) : DerivedSurfaceProperty ( prop, ss, surf, g ) { 
-
-      double value = 0.0;
-
-      for( int i = g->firstI( true); i < g->lastI (true ) + 1; ++ i ) {
-
-         for( int j = g->firstJ(true); j < g->lastJ(true) + 1; ++j ) {
-            set( i, j, value ++ );
+   double surfaceValue   = 0.0;
+   double formationValue = 0.0;
+   for( int i = testGrid.firstI( true ); i < testGrid.lastI( true ) + 1; ++ i ) {
+      for( int j = testGrid.firstJ( true ); j < testGrid.lastJ( true ) + 1; ++ j ) {
+         s.set(  i, j, surfaceValue  );
+         sf.set( i, j, surfaceValue ++ );
+         for( int k = f.firstK(); k < f.lastK() + 1; ++ k ) {
+            f.set( i, j, k, formationValue ++ );
          }
       }
    }
-
-};
-
-
-TEST( DerivedSurfaceProperty, Test1 )
-{
-   TestAbstractGrid     testGrid;
-   TestAbstractSurface  testSurface;
-   TestAbstractSnapshot testSnapshot;
-   TestAbstractProperty testProperty;
-
-   TestSurfaceProperty m ( &testProperty, &testSnapshot, &testSurface, &testGrid );
-
-   EXPECT_DOUBLE_EQ( 24.0, m.get ( 3, 3 ) );
+   EXPECT_DOUBLE_EQ( 24.0,  s.get ( 3, 3 ) );
+   EXPECT_DOUBLE_EQ( 243.0, f.get ( 3, 3, 3 ));
+   EXPECT_DOUBLE_EQ( 17.0,  sf.get( 2, 3 ) );
 }
