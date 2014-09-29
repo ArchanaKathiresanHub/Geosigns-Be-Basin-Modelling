90d877d7017e6729b889423aaff109f38fb1ad62
w.suijlen <w.suijlen@pds.tfs.local>
2014-09-29 15:02:59 +0000

    Plots are now generated and saved to build drop folder
    work-items: #31374, #33883
    git-tfs-id: [https://sede-pt-ce2.visualstudio.com/DefaultCollection]$/Basin Modeling/IBS/Trunk;C45922

--- development/applications/hpc/periodic_benchmark/Linux-RunBenchmark.sh
+++ development/applications/hpc/periodic_benchmark/Linux-RunBenchmark.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 src=${SRC_DIR:-`dirname $0`/../../../..}
 build=${BUILD_DIR:-`mktemp -d`}
+installdir=${INSTALL_DIR:-${build}}
 platform=${PLATFORM:-Linux}
 configuration=${CONFIGURATION:-Release}
 tfs_version=${TFS_VERSION:-YYYY}
@@ -9,12 +10,17 @@ nprocs=${PROCESSORS:-1}
 tabledir=${TABLE_DIR:-.}
 tablefile=${TABLE_FILE:-table}
 
+
+# Make temporary files for scripts
+selection=`mktemp`
+plot=`mktemp`
+
 # Set some code to execute when shell script exits
 function onExit()
 {
   # remove the temporary directory made for the build directory
   if [ "x${BUILD_DIR}" = x ]; then
-     rm -Rf $build
+     rm -Rf $build $selection $plot
   fi
 }
 
@@ -32,18 +38,22 @@ pushd ${tabledir} > /dev/null
 tabledir=`pwd -P`
 popd > /dev/null
 
+
+# Define project
+project=Barrowlands
+
 # Fastcauldron is already built and available in the build direction.
 current_time=$(date +'%Y-%m-%d %H:%M:%S')
 
 # Build Cauldron
 build_success=ok
-GEOCASE=False Deploy=False BUILD_DIR=${build} ${src}/development/BuildProjects/Linux-BuildAndInstall.sh \
+GEOCASE=False DEPLOY=False BUILD_DIR=${build} ${src}/development/BuildProjects/Linux-BuildAndInstall.sh \
    || { echo "warning: Build has not completed succesfully"; build_success=failing; }
 
 # Generate the projects first
 echo ">>> Generating benchmark projects <<<"
-cp -R ${src}/development/applications/hpc/intel_benchmark_projects/Barrowlands ${build}
-pushd ${build}/Barrowlands > /dev/null
+cp -R ${src}/development/applications/hpc/intel_benchmark_projects/$project ${build}
+pushd ${build}/$project > /dev/null
   bash generate.sh
 popd  > /dev/null
 
@@ -54,10 +64,10 @@ for size in micro small
 do
    for difficulty in easy medium hard salt
    do
-      pushd Barrowlands/$size-$difficulty > /dev/null
+      pushd $project/$size-$difficulty > /dev/null
       logfile=output.log
       export TIMEFORMAT="%R"
-      echo ">>> Running fastcauldron $simulator_options on Barrowlands of size $size and difficulty $difficulty <<<"
+      echo ">>> Running fastcauldron $simulator_options on $project of size $size and difficulty $difficulty <<<"
       T_sim=$( ( time mpirun -np $nprocs ${build}/applications/fastcauldron/fastcauldron -project Project.project3d -verbose $simulator_options > $logfile 2>&1 ; ) 2>&1 )
 
       echo ">>> Adding results to table ${tabledir}/${tablefile} <<<"
@@ -80,7 +90,7 @@ do
         echo -ne "${platform}\t"
         echo -ne "${configuration}\t"
         echo -ne "${nprocs}\t"
-        echo -ne "Barrowlands\t"
+        echo -ne "${project}\t"
         echo -ne "${size}\t"
         echo -ne "${difficulty}\t"
         echo -ne "${T_sim}\t"
@@ -90,9 +100,31 @@ do
         echo -ne "\n"
       ) >>  ${tabledir}/${tablefile}
 
+      # Extract data from the graph
+      sed -ne "/ok\\t${platform}\\t${configuration}\\t${nprocs}\\t${project}\\t${size}\\t${difficulty}/p" < ${tabledir}/${tablefile} > $selection
+       
+      # Write the Gnuplot script
+      Xaxis=2
+cat > $plot <<EOF
+set terminal png
+set output "${installdir}/perfhist-$project-$size-$difficulty-on-$platform-$configuration-$nprocs.png"
+set datafile separator "\t"
+set xlabel "TFS Changeset"
+set ylabel "Running time (seconds)"
+set yrange [0:]
+set title "The $size sized $difficulty difficulty project $project simulated\n with $simulator_options on $platform built as $configuration on $nprocs processors" 
+plot "$selection" using ${Xaxis}:10 title "Total time" with linespoints,\
+     "$selection" using ${Xaxis}:11 title "System assembly time" with linespoints,\
+     "$selection" using ${Xaxis}:12 title "System solve time" with linespoints,\
+     "$selection" using ${Xaxis}:13 title "Other" with linespoints
+EOF
+
+      gnuplot < $plot
+
       popd > /dev/null
    done
 done
+
 
 
 popd
