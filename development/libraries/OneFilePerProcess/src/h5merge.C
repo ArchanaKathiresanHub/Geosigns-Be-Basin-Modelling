42f8c271c96da497d905fd9d6364ee157cd9b031
Natalya Kuznetsova <natalya.kuznetsova@pds.nl>
2014-10-14 14:51:53 +0000

    Missed in last check-in
    work-items: #35327
    git-tfs-id: [https://sede-pt-ce2.visualstudio.com/DefaultCollection]$/Basin Modeling/IBS/Trunk;C46217

--- development/libraries/OneFilePerProcess/src/h5merge.C
+++ development/libraries/OneFilePerProcess/src/h5merge.C
@@ -22,7 +22,6 @@
 
 herr_t readDataset ( hid_t groupId, const char* name, void * voidReader)  {
 
-
    FileHandler* reader = static_cast<FileHandler *>( voidReader );
 
    if( groupId == NULL ) {
@@ -60,9 +59,19 @@ herr_t readDataset ( hid_t groupId, const char* name, void * voidReader)  {
          if( reader->m_rank == 0 ) {       
             // Greate a group in the global file
             startTime = MPI_Wtime();
-            if( !reader->m_reuse ) {
-               reader->m_groupId = H5Gcreate( reader->m_globalFileId, name,  H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT ); 
-            } 
+//               // open a group
+//               reader->m_groupId = H5Gopen( reader->m_globalFileId, name, H5P_DEFAULT ); 
+               // create a group if it doesn't exist
+//               if( reader->m_groupId < 0 ) { 
+//                  reader->m_groupId = H5Gcreate( reader->m_globalFileId, name,  H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT ); 
+//               } 
+ //           } else {
+               if( !reader->m_reuse ) {
+                  reader->m_groupId = H5Gcreate( reader->m_globalFileId, name,  H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT ); 
+ //              } else {
+                  //   reader->m_groupId = H5Gopen( reader->m_globalFileId, name, H5P_DEFAULT ); //&
+ //             }
+            }
             if( reader->m_groupId < 0 ) {
                std::cout << " ERROR Cannot open or create the group " << name << std::endl;
             }
@@ -137,7 +146,9 @@ herr_t readDataset ( hid_t groupId, const char* name, void * voidReader)  {
       }
    } else {
       startTime = MPI_Wtime();
+
       MPI_Reduce( reader->m_data.data(), reader->m_sumData.data(), reader->m_valCount, MPI_CHAR, MPI_SUM, 0, reader->m_comm );
+
       FileHandler::s_collectingTime +=  MPI_Wtime() - startTime;
    }
 
@@ -216,6 +227,219 @@ herr_t readDataset ( hid_t groupId, const char* name, void * voidReader)  {
    return 0;
 }
 
+herr_t updateDataset ( hid_t groupId, const char* name, void * voidReader)  {
+
+
+   FileHandler* reader = static_cast<FileHandler *>( voidReader );
+
+   if( groupId == NULL ) {
+      
+      if( reader->m_rank == 0 ) {       
+         std::cout << " ERROR Cannot iterate the group or dataset " << name << std::endl;
+      }
+      return -1;
+   }
+
+   hid_t memspace, filespace;
+   hid_t local_dset_id;
+   hid_t dataSpaceId;
+   int   status = 0;
+ 
+   H5G_stat_t statbuf;
+
+   double startTime = MPI_Wtime();
+   status = H5Gget_objinfo ( groupId, name, 0, &statbuf );
+   FileHandler::s_readDTime += MPI_Wtime() - startTime;
+
+   if( reader->checkError( status ) < 0 ) {
+      if( reader->m_rank == 0 ) {       
+         std::cout << " ERROR Cannot get the object " <<  name << " info" << std::endl;
+      }
+      return -1;
+   }
+
+   bool update = true;
+
+   switch ( statbuf.type ) {
+   case H5G_GROUP:
+      {
+         std::stringstream groupName;
+         groupName << "/" << name;
+
+         if( reader->m_rank == 0 ) {       
+            // Greate a group in the global file
+            startTime = MPI_Wtime();
+            // open existing group for update
+            reader->m_groupId = H5Gopen( reader->m_globalFileId, name, H5P_DEFAULT ); 
+            // create a group if it doesn't exist
+            if( reader->m_groupId < 0 ) { 
+               reader->m_groupId = H5Gcreate( reader->m_globalFileId, name,  H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT ); 
+               update = false;
+            } 
+            if( reader->m_groupId < 0 ) {
+               std::cout << " ERROR Cannot open or create the group " << name << std::endl;
+            }
+            FileHandler::s_writeDTime += MPI_Wtime() - startTime;
+            FileHandler::s_creatingGTime += MPI_Wtime() - startTime;
+         }
+         // Iterate over the members of the group
+         H5Giterate ( reader-> m_localFileId, groupName.str().c_str(), 0, updateDataset, voidReader );
+
+         if( reader->m_rank == 0 ) {       
+            H5Gclose( reader->m_groupId );
+         }
+         return 0;
+      }
+      break;
+   case H5G_DATASET:
+      break;
+   case H5G_TYPE:
+      break;
+   default: ;
+
+   } 
+
+   startTime = MPI_Wtime();
+   local_dset_id = H5Dopen ( groupId, name, H5P_DEFAULT );
+
+#ifdef SAFE_RUN
+   if( reader->checkError ( local_dset_id ) < 0 ) {
+      return -1;
+   }
+#endif
+
+   dataSpaceId = H5Dget_space ( local_dset_id );
+
+   reader->m_spatialDimension = H5Sget_simple_extent_dims ( dataSpaceId, reader->m_dimensions, 0 );
+
+   H5Sclose( dataSpaceId );
+ 
+   // Get the dataset datatype
+   hid_t   dtype    = H5Dget_type( local_dset_id );
+   ssize_t dataSize = H5Tget_size(dtype);
+ 
+#ifdef SAFE_RUN
+   if( reader->checkError ( dataSize ) < 0 || dataSize < 0 ) {
+      return -1;
+   }
+#endif
+
+    // Allocate data buffers
+   reader->reallocateBuffers ( dataSize );
+
+   // Read the local data
+   memspace = H5Screate_simple( reader->m_spatialDimension, reader->m_count, NULL ); 
+   filespace = H5Dget_space( local_dset_id );
+
+   H5Sselect_hyperslab( filespace, H5S_SELECT_SET, reader->m_offset, NULL, reader->m_count, NULL );
+
+   double startTime1 = MPI_Wtime();
+   H5Dread ( local_dset_id, dtype, memspace, filespace, H5P_DEFAULT, reader->m_data.data() );
+   FileHandler::s_readingDTime += MPI_Wtime() - startTime1;
+   FileHandler::s_readDTime += MPI_Wtime() - startTime;
+
+   if( !reader->m_reuse ) {
+      H5Sclose( filespace );
+      H5Sclose( memspace );
+   }
+   
+   // Summarize all local data in a global array
+   if( reader->m_spatialDimension == 1 ) {
+      if( reader->m_rank == 0 ) {
+         reader->m_sumData = reader->m_data;
+      }
+   } else {
+      startTime = MPI_Wtime();
+
+      MPI_Reduce( reader->m_data.data(), reader->m_sumData.data(), reader->m_valCount, MPI_CHAR, MPI_SUM, 0, reader->m_comm );
+
+      FileHandler::s_collectingTime +=  MPI_Wtime() - startTime;
+   }
+
+   if( reader->m_rank == 0 ) {
+      startTime1 = MPI_Wtime();
+
+      hid_t global_dset_id;
+      if( !reader->m_reuse ) {
+         // Write the global data into the global file
+         memspace = H5Screate_simple( reader->m_spatialDimension, reader->m_count, NULL ); 
+         
+         startTime = MPI_Wtime();
+         
+         if( reader->m_groupId != H5P_DEFAULT ) {
+            // Dataset is under the sub-group
+            global_dset_id = H5Dopen ( reader->m_groupId, name, H5P_DEFAULT );
+            if( global_dset_id < 0 ) {
+               global_dset_id = H5Dcreate( reader->m_groupId, name, dtype, memspace, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT );
+            }
+         } else {
+            // Dataset is under the main group
+            global_dset_id = H5Dopen ( reader->m_globalFileId, name, H5P_DEFAULT ); 
+            if( global_dset_id < 0 ) {
+               global_dset_id = H5Dcreate( reader->m_globalFileId, name, dtype, memspace, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT );
+            }
+         }
+         FileHandler::s_creatingDTime +=  MPI_Wtime() - startTime;
+         
+         if( global_dset_id < 0 ) {
+            std::cout << " ERROR Cannot create the dataset " << name << std::endl;
+         }
+      } else {
+         global_dset_id = local_dset_id;
+      }
+
+      if( !reader->m_reuse ) {
+         filespace = H5Dget_space( global_dset_id );
+         H5Sselect_hyperslab( filespace, H5S_SELECT_SET, reader->m_offset, NULL, reader->m_count, NULL );
+      }
+      startTime = MPI_Wtime();
+      status = H5Dwrite( global_dset_id, dtype, memspace, filespace, H5P_DEFAULT, reader->m_sumData.data() );
+      FileHandler::s_writingDTime +=  MPI_Wtime() - startTime;
+      FileHandler::s_writeDTime +=  MPI_Wtime() - startTime1;
+
+      if( status >= 0 ) {
+         
+         startTime = MPI_Wtime();
+         if( !reader->m_reuse ) {
+            if( !update ) {
+               status = reader->readAttributes( local_dset_id, global_dset_id ); 
+               if( status < 0 ) {
+                  std::cout << " ERROR Cannot write attributes " << std::endl;
+               }
+               FileHandler::s_attributeTime +=  MPI_Wtime() - startTime;
+            }
+         }
+      } else {
+         if( reader->m_rank == 0 ) {       
+            std::cout << " ERROR Cannot write the dataset " << name << std::endl;
+         }
+      }
+
+      startTime = MPI_Wtime();
+
+      H5Sclose( filespace );
+      H5Sclose( memspace );
+
+      if( !reader->m_reuse ) {
+         H5Dclose( global_dset_id );
+      }
+      FileHandler::s_writeDTime +=  MPI_Wtime() - startTime;
+   }
+ 
+   H5Dclose( local_dset_id );
+
+#ifdef SAFE_RUN
+   MPI_Bcast( &status, 1, MPI_INT, 0, reader->m_comm );
+   
+   if( status < 0 ) {
+      return -1;
+   }
+#endif
+   
+   
+   return 0;
+}
+
 bool mergeFiles( MPI_Comm comm, const std::string & fileName, const std::string & tempDirName, const bool reuse ) {
  
    double totalStart = MPI_Wtime();
@@ -224,7 +448,6 @@ bool mergeFiles( MPI_Comm comm, const std::string & fileName, const std::string 
    hid_t status = 0, close_status = 0, iteration_status = 0;
 
    reader.m_reuse = reuse;
-
    hid_t fileAccessPList = H5Pcreate(H5P_FILE_ACCESS);
 
    std::stringstream tmpName;
@@ -234,11 +457,12 @@ bool mergeFiles( MPI_Comm comm, const std::string & fileName, const std::string 
    H5Pset_fapl_ofpp ( fileAccessPList, comm, tmpName.str().c_str(), 0 );  
    
    if( reader.m_reuse ) {
+      // only rank 0 needs RDWR access!
       reader.m_localFileId = H5Fopen( fileName.c_str(), H5F_ACC_RDWR, fileAccessPList );
    } else {
       reader.m_localFileId = H5Fopen( fileName.c_str(), H5F_ACC_RDONLY, fileAccessPList );
    }
-
+   
    H5Pclose( fileAccessPList );
    FileHandler::s_readDTime += MPI_Wtime() - startTime;
    
@@ -251,7 +475,6 @@ bool mergeFiles( MPI_Comm comm, const std::string & fileName, const std::string 
   
    if( reader.m_rank == 0 ) {
       startTime = MPI_Wtime();   
-
       if( reader.m_reuse ) {
          reader.m_globalFileId = reader.m_localFileId; 
       } else {
@@ -311,6 +534,95 @@ bool mergeFiles( MPI_Comm comm, const std::string & fileName, const std::string 
    return true;
 }
 
+bool appendFiles( MPI_Comm comm, const std::string & fileName, const std::string & tempDirName ) {
+ 
+   double totalStart = MPI_Wtime();
+   FileHandler reader ( comm );
+ 
+   hid_t status = 0, close_status = 0, iteration_status = 0;
+
+   reader.m_reuse = false;
+
+   hid_t fileAccessPList = H5Pcreate(H5P_FILE_ACCESS);
+
+   std::stringstream tmpName;
+   tmpName << tempDirName << "/{NAME}_{MPI_RANK}";
+
+   double startTime = MPI_Wtime();
+   H5Pset_fapl_ofpp ( fileAccessPList, comm, tmpName.str().c_str(), 0 );  
+   
+   reader.m_localFileId = H5Fopen( fileName.c_str(), H5F_ACC_RDONLY, fileAccessPList );
+
+   H5Pclose( fileAccessPList );
+   FileHandler::s_readDTime += MPI_Wtime() - startTime;
+   
+   if( reader.checkError( reader.m_localFileId ) < 0 ) {
+      if( reader.m_rank == 0 ) {       
+         std::cout << " ERROR Cannot open the local file " << fileName << std::endl;
+      }
+      return false;
+   }
+  
+   if( reader.m_rank == 0 ) {
+      startTime = MPI_Wtime();   
+      // Update existing file
+      reader.m_globalFileId = H5Fopen( fileName.c_str(), H5F_ACC_RDWR, H5P_DEFAULT ); 
+      //If it doesn't exist create one
+      if(  reader.m_globalFileId  < 0 ) {
+         reader.m_globalFileId = H5Fcreate( fileName.c_str(), H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT );
+      }
+      FileHandler::s_writeDTime += MPI_Wtime() - startTime;
+
+      if( reader.m_globalFileId < 0 ) {
+         std::cout << " ERROR Cannot open or create the global file " << fileName << std::endl;
+         status = 1; 
+      }
+   }
+   
+   startTime = MPI_Wtime();   
+   MPI_Bcast( &status, 1, MPI_INT, 0, comm );
+   FileHandler::s_collectingTime += MPI_Wtime() - startTime;
+
+   if( status > 0 ) {
+      // global file cannot be created
+      H5Fclose( reader.m_localFileId ); 
+      if( reader.m_rank == 0 ) {       
+         std::cout << " ERROR Cannot close the local file " << fileName << std::endl;
+      }
+      return false;
+   }
+   // Iterate over the memebers of the local file
+    
+   iteration_status = H5Giterate ( reader.m_localFileId, "/", 0, updateDataset, &reader );
+
+   startTime = MPI_Wtime();
+   status = H5Fclose( reader.m_localFileId );    
+   FileHandler::s_readDTime += MPI_Wtime() - startTime;
+  
+   close_status = reader.checkError( status ); 
+    
+   if( reader.m_rank == 0 ) {
+      startTime = MPI_Wtime();   
+      status = H5Fclose( reader.m_globalFileId );  
+      FileHandler::s_writeDTime += MPI_Wtime() - startTime;
+   }
+ 
+   startTime = MPI_Wtime();   
+   MPI_Bcast( &status, 1, MPI_INT, 0, comm );
+   FileHandler::s_collectingTime += MPI_Wtime() - startTime;
+
+   FileHandler::s_totalTime += MPI_Wtime() - totalStart;
+   
+   if( status < 0 || close_status < 0 || iteration_status < 0 ) {
+      if( reader.m_rank == 0 ) {       
+         std::cout << " ERROR Cannot exit while merging the file " << fileName << std::endl;
+      }
+      return false;
+   }
+   
+   return true;
+}
+
 bool copyFile( std::string & dstPath, std::string & curPath )    
 {
    bool status = true;
@@ -341,3 +653,4 @@ bool copyFile( std::string & dstPath, std::string & curPath )
 
    return status;
 }
+
