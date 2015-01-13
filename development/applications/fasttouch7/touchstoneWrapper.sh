1acbd581bf5965256a7b24b87910fed8b5021775
Luca Carniato <luca.carniato@pds.nl>
2015-01-13 13:04:50 +0000

    disable standard output and error from touchstone wrapper
    work-items: #39825
    git-tfs-id: [https://sede-pt-ce2.visualstudio.com/DefaultCollection]$/Basin Modeling/IBS/Trunk;C48179

--- development/applications/fasttouch7/touchstoneWrapper.sh
+++ development/applications/fasttouch7/touchstoneWrapper.sh
@@ -9,5 +9,6 @@ MCR_ROOT="${MATLABMCR}"
 export XAPPLRESDIR="${MCR_ROOT}/X11/app-defaults" 
 export LD_LIBRARY_PATH="${MCR_ROOT}/runtime/glnxa64:${MCR_ROOT}/bin/glnxa64:${MCR_ROOT}/sys/os/glnxa64:${MCR_ROOT}/sys/java/jre/glnxa64/jre/lib/amd64/native_threads:${MCR_ROOT}/sys/java/jre/glnxa64/jre/lib/amd64/server:${MCR_ROOT}/sys/java/jre/glnxa64/jre/lib/amd64:${TSLIB_LIBRARY_DIR}:\$LD_LIBRARY_PATH"
 export LANG="en_US.UTF-8" 
+export DISPLAY=
 
-$dir/touchstoneWrapper "$@"
+$dir/touchstoneWrapper "$@" > /dev/null 2>&1
