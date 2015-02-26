25eb1b784e611d2f569be0f8b1f5a6dfa7733da6
William Senior <william.senior@pds.nl>
2015-02-26 14:37:35 +0000

    Fix for the windows check-in build.
    work-items: #43776
    git-tfs-id: [https://sede-pt-ce2.visualstudio.com/DefaultCollection]$/Basin Modeling/IBS/Trunk;C49395

--- development/libraries/LinearAlgebra/test/SimdInstTest02.C
+++ development/libraries/LinearAlgebra/test/SimdInstTest02.C
@@ -2,17 +2,42 @@
 #include "../src/SimdInstruction.h"
 #include <gtest/gtest.h>
 #include <stdlib.h>
-#include <tr1/array>
 
-#include <iostream>
+#ifndef _WIN32
+#include <tr1/array>
+#endif
+
+
+#ifdef _WIN32
+template<typename Type, const unsigned int Size>
+class array {
+public :
+
+   const Type& operator []( const unsigned int i ) const {
+      return m_values [ i ];
+   }
+
+   Type& operator []( const unsigned int i ) {
+      return m_values [ i ];
+   }
+
+private :
+
+   Type m_values [ Size ];
+
+};
+#else
 using namespace std;
+using namespace tr1;
+#endif
+
 
 TEST ( SimdInstrTests, NoSimdTest02 ) {
 
    typedef Numerics::SimdTraits<Numerics::NO_SIMD> SimdTraits;
    typedef Numerics::SimdInstruction<Numerics::NO_SIMD> SimdInstruction;
 
-   typedef std::tr1::array<double, SimdTraits::DoubleStride> DoubleArray;
+   typedef array<double, SimdTraits::DoubleStride> DoubleArray;
 
    double value1 = 2.5;
    double value2 = 3.25;
@@ -51,7 +76,7 @@ TEST ( SimdInstrTests, SseTest02 ) {
    typedef Numerics::SimdInstruction<SimdUsed> SimdInstruction;
 
 
-   typedef std::tr1::array<double, SimdTraits::DoubleStride> DoubleArray;
+   typedef array<double, SimdTraits::DoubleStride> DoubleArray;
 
    double value1 = 2.5;
    double value2 = 3.25;
@@ -93,7 +118,7 @@ TEST ( SimdInstrTests, AvxTest02 ) {
    typedef Numerics::SimdInstruction<SimdUsed> SimdInstruction;
 
 
-   typedef std::tr1::array<double, SimdTraits::DoubleStride> DoubleArray;
+   typedef array<double, SimdTraits::DoubleStride> DoubleArray;
 
    double value1 = 2.5;
    double value2 = 3.25;
