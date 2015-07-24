e6fe347ba8276be697a6cabd264364fda8bdf7a9
William Senior <william.senior@pds.nl>
2015-07-24 08:38:01 +0000

    Added some comments.
    work-items: #49677
    git-tfs-id: [https://sede-pt-ce2.visualstudio.com/DefaultCollection]$/Basin Modeling/IBS/Trunk;C52600

--- development/libraries/DerivedProperties/src/ReflectivitySurfaceCalculator.h
+++ development/libraries/DerivedProperties/src/ReflectivitySurfaceCalculator.h
@@ -12,25 +12,41 @@
 
 namespace DerivedProperties {
 
-   /// 
+   /// \brief Calculator for the reflectivity of a surface.
    class  ReflectivitySurfaceCalculator : public SurfacePropertyCalculator {
 
    public :
 
+      /// \brief Constructor.
       ReflectivitySurfaceCalculator ( const GeoPhysics::ProjectHandle* projectHandle );
 
+      /// \brief Calculate the reflectivity for the surface.
+      ///
+      /// \param [in]  propManager The property manager.
+      /// \param [in]  snapshot    The snapshot time for which the reflectivity is requested.
+      /// \param [in]  surface     The surface for which the reflectivity is requested.
+      /// \param [out] derivedProperties On exit will contain a single surface property, the reflectivity of the surface.
+      /// \pre snapshot points to a valid snapshot age.
+      /// \pre surface points to a valid surface.
       virtual void calculate ( AbstractPropertyManager&            propManager,
                                const DataModel::AbstractSnapshot*  snapshot,
                                const DataModel::AbstractSurface*   surface,
                                      SurfacePropertyList&          derivedProperties ) const;
 
       /// \brief Determine if the property is computable for the specific combination of surface and snapshot.
+      ///
+      /// \param [in]  propManager The property manager.
+      /// \param [in]  snapshot    The snapshot time for which the reflectivity is requested.
+      /// \param [in]  surface     The surface for which the reflectivity is requested.
+      /// \pre snapshot points to a valid snapshot age or is null.
+      /// \pre surface points to a valid surface or is null.
       virtual bool isComputable ( const AbstractPropertyManager&     propManager,
                                   const DataModel::AbstractSnapshot* snapshot,
                                   const DataModel::AbstractSurface*  surface ) const;
 
    private :
 
+      /// \brief The project handle.
       const GeoPhysics::ProjectHandle* m_projectHandle;
 
     };
