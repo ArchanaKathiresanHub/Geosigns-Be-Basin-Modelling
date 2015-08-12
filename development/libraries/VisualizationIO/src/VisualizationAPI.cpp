c1500ca55accfa6aa7b0a80e5645686e63c116d0
Sergey Koshelev <sergey.koshelev@pds.nl>
2015-08-12 07:11:26 +0000

    Fix build on RHEL6
    work-items: #49956
    git-tfs-id: [https://sede-pt-ce2.visualstudio.com/DefaultCollection]$/Basin Modeling/IBS/Trunk;C53009

--- development/libraries/VisualizationIO/src/VisualizationAPI.cpp
+++ development/libraries/VisualizationIO/src/VisualizationAPI.cpp
@@ -1,7 +1,20 @@
+//
+// Copyright (C) 2012-2015 Shell International Exploration & Production.
+// All rights reserved.
+//
+// Developed under license for Shell by PDS BV.
+//
+// Confidential and proprietary source code of Shell.
+// Do not distribute without written permission from Shell.
+//
+
 #include "VisualizationAPI.h"
-#include <new>
+
 #include <assert.h>
+
+#include <new>
 #include <algorithm>
+#include <cstring>
 
 using namespace CauldronIO;
 
