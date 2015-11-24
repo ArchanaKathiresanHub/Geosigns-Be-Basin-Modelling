628c6302de28e943580088fbf1646fccb159576a
William Senior <william.senior@pds.nl>
2015-11-24 09:13:24 +0000

    First time under version control for classes to map new solution vectors to and from current 3d strucutre and to contain the boundary conditions.
    work-items: #53937, #53951
    git-tfs-id: [https://sede-pt-ce2.visualstudio.com/DefaultCollection]$/Basin Modeling/IBS/Trunk;C55050

---
+++ development/applications/fastcauldron/src/BoundaryConditions.C
@@ -0,0 +1,26 @@
+//                                                                      
+// Copyright (C) 2015 Shell International Exploration & Production.
+// All rights reserved.
+// 
+// Developed under license for Shell by PDS BV.
+// 
+// Confidential and proprietary source code of Shell.
+// Do not distribute without written permission from Shell.
+// 
+#include "BoundaryConditions.h"
+
+void BoundaryConditions::reset () {
+
+   for ( int i = 0; i < NumberOfElementNodes; ++i ) {
+      m_bcs [ i ] = Interior_Boundary;
+      m_bcValues [ i ] = CAULDRONIBSNULLVALUE;
+   }
+
+}
+
+void BoundaryConditions::setBoundaryConditions ( const int                 node, 
+                                                 const Boundary_Conditions bc,
+                                                 const double              bcValue ) {
+   m_bcs [ node ] = bc;
+   m_bcValues [ node ] = bcValue;
+}
