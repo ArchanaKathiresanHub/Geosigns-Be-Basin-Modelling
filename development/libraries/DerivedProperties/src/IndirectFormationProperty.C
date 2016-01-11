3085ac0bc62c9ea264bc2257834cb04fb76c2f98
Massimiliano Beccaria <massimiliano.beccaria@pds.nl>
2016-01-11 13:57:15 +0000

    Newly added classes: IndirectFormationProperty and VesHighResFormationCalculator
    work-items: #55214
    git-tfs-id: [https://sede-pt-ce2.visualstudio.com/DefaultCollection]$/Basin Modeling/IBS/Trunk;C56052

---
+++ development/libraries/DerivedProperties/src/IndirectFormationProperty.C
@@ -0,0 +1,19 @@
+#include "IndirectFormationProperty.h"
+#include "Interface/Interface.h"
+
+DerivedProperties::IndirectFormationProperty::IndirectFormationProperty(const DataModel::AbstractProperty * property,
+                                                                              FormationPropertyPtr          propertyValues) :
+   FormationProperty( property,
+                      propertyValues->getSnapshot(),
+                      propertyValues->getFormation(),
+                      propertyValues->getGrid(),
+                      propertyValues->lengthK() ),
+   m_property(propertyValues)
+{
+}
+
+
+double DerivedProperties::IndirectFormationProperty::getUndefinedValue () const
+{
+   return DataAccess::Interface::DefaultUndefinedMapValue;
+}
