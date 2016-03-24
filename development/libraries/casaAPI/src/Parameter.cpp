d81ad154fe835ec4a8989d1fc5fbf2e913ca0e18
Luca Carniato <luca.carniato@pds.nl>
2016-03-24 12:18:50 +0000

    small correction in the name of the lithofraction parameter
    work-items: #57183
    git-tfs-id: [https://sede-pt-ce2.visualstudio.com/DefaultCollection]$/Basin Modeling/IBS/Trunk;C58002

--- development/libraries/casaAPI/src/Parameter.cpp
+++ development/libraries/casaAPI/src/Parameter.cpp
@@ -54,7 +54,7 @@ casa::Parameter * casa::Parameter::load( CasaDeserializer & dz, const char * obj
    else if ( ot == "PrmPermeabilityModel"            ) { return new PrmPermeabilityModel(            dz, vr ); }
    else if ( ot == "PrmLithoSTPThermalCond"          ) { return new PrmLithoSTPThermalCond(          dz, vr ); }
    else if ( ot == "PrmWindow"                       ) { return new PrmWindow(                       dz, vr ); }
-   else if ( ot == "PrmLithoFraction               " ) { return new PrmLithoFraction(                dz, vr ); }
+   else if ( ot == "PrmLithoFraction"                ) { return new PrmLithoFraction(                dz, vr ); }
    else
    {
       throw ErrorHandler::Exception( ErrorHandler::DeserializationError )
