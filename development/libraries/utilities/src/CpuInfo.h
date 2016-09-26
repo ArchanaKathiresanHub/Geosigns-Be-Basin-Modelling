a70a740facdfa71e0e75e195288dbb00f0b5f738
Luca Carniato <luca.carniato@pds.nl>
2016-09-26 07:50:13 +0000

    cpuInfo class works also in windows (using __cpuid)
    work-items: #61146
    git-tfs-id: [https://sede-pt-ce2.visualstudio.com/DefaultCollection]$/Basin Modeling/IBS/Trunk;C62083

--- development/libraries/utilities/src/CpuInfo.h
+++ development/libraries/utilities/src/CpuInfo.h
@@ -1,39 +1,43 @@
 #ifndef _CPU_INFO_H_
 #define _CPU_INFO_H_
 
-// This class gets the info of the current cpu and the supported instruction sets
+#if defined(_WIN32) || defined (_WIN64)
+#include <intrin.h>
+typedef unsigned __int32  uint32_t;
+#else
+#include <stdint.h>
+#endif
+
+// This class gets CPU info and the supported instructions both in windows and linux
 
 class cpuInfo
 {
 public:
 
    cpuInfo();
-   int getFamily()          { return m_family;}
-   int getModel()           { return m_model;}
-   int getStepping()        { return m_stepping;}
-      
-   bool supportSse()        { return m_supportSse; }
-   bool supportSse2()       { return m_supportSse2; }
-   bool supportSse3()       { return m_supportSse3; }
-   bool supportSse42()      { return m_supportSse42; }
-   bool supportAvx()        { return m_supportAvx; }
-   bool supportFma3( )      { return m_supportFma3; }
+   int getFamily() const          { return m_family; }
+   int getModel() const           { return m_model; }
+   int getStepping() const        { return m_stepping; }
 
-   bool supportAvx2()       { return m_supportAvx2; }
-   bool supportAvx512()     { return m_supportAvx512; }
-   bool supportAvx512Ifma() { return m_supportAvx512Ifma; }
-   bool supportAvx512Er( )  { return m_supportAvx512er; }
+   bool supportSse() const        { return m_supportSse; }
+   bool supportSse2() const       { return m_supportSse2; }
+   bool supportSse3() const       { return m_supportSse3; }
+   bool supportSse42() const      { return m_supportSse42; }
+   bool supportAvx() const        { return m_supportAvx; }
+   bool supportFma3() const       { return m_supportFma3; }
 
-   char * getVendor()       { return m_vendorString; }
+   bool supportAvx2() const       { return m_supportAvx2; }
+   bool supportAvx512() const     { return m_supportAvx512; }
+   bool supportAvx512Ifma() const { return m_supportAvx512Ifma; }
+   bool supportAvx512Er() const   { return m_supportAvx512er; }
+
+   char * getVendor()             { return m_vendorString; }
 
 private:
 
    int getBit( unsigned int num, int position );
 
-#if not defined(_WIN32) && not defined (_WIN64)
-   void native_cpuid( unsigned int *eax, unsigned int *ebx,
-      unsigned int *ecx, unsigned int *edx );
-#endif
+   void native_cpuid( uint32_t * regs, unsigned eax );
 
    int  m_family;
    int  m_model;
@@ -55,94 +59,84 @@ private:
 };
 
 //Implementation
-cpuInfo::cpuInfo( ): m_supportSse( false ),
-                     m_supportSse2( false ),
-                     m_supportSse3( false ),
-                     m_supportSse42( false ),
-                     m_supportAvx( false ),
-                     m_supportFma3( false ),
+cpuInfo::cpuInfo() : m_supportSse( false ),
+m_supportSse2( false ),
+m_supportSse3( false ),
+m_supportSse42( false ),
+m_supportAvx( false ),
+m_supportFma3( false ),
 
-                     m_supportAvx2( false ),
-                     m_supportAvx512( false ),
-                     m_supportAvx512Ifma( false ),
-                     m_supportAvx512er( false )
+m_supportAvx2( false ),
+m_supportAvx512( false ),
+m_supportAvx512Ifma( false ),
+m_supportAvx512er( false )
 {
-#if not defined(_WIN32) && not defined (_WIN64)
-   unsigned int level;
-   unsigned int eax;
-   unsigned int ebx;
-   unsigned int ecx;
-   unsigned int edx;
+   // the registers regs[0] = eax, regs[1] = ebx, regs[2] = ecx, regs[3] = edx 
+   uint32_t regs[4];
 
-   // Get information from the registers (highest function parameter)
-   eax = 0;
-   native_cpuid( &eax, &ebx, &ecx, &edx );
+   // Get information from the registers (eax = 0, highest function parameter)
+   native_cpuid( regs, 0 );
 
    // If CPU is Intel then
    // the first  4 characters are Genu
    // the second 4 characters are ineI
    // the third  4 characters are ntel
-   m_vendorString[0] = static_cast<char>( ebx & 0xFF );
-   m_vendorString[1] = static_cast<char>( ( ebx >> 8 ) & 0xFF );
-   m_vendorString[2] = static_cast<char>( ( ebx >> 16 ) & 0xFF );
-   m_vendorString[3] = static_cast<char>( ( ebx >> 24 ) & 0xFF );
+   m_vendorString[0] = static_cast<char>( regs[1] & 0xFF );
+   m_vendorString[1] = static_cast<char>( ( regs[1] >> 8 ) & 0xFF );
+   m_vendorString[2] = static_cast<char>( ( regs[1] >> 16 ) & 0xFF );
+   m_vendorString[3] = static_cast<char>( ( regs[1] >> 24 ) & 0xFF );
 
-   m_vendorString[4] = static_cast<char>( edx & 0xFF );
-   m_vendorString[5] = static_cast<char>( ( edx >> 8 ) & 0xFF );
-   m_vendorString[6] = static_cast<char>( ( edx >> 16 ) & 0xFF );
-   m_vendorString[7] = static_cast<char>( ( edx >> 24 ) & 0xFF );
+   m_vendorString[4] = static_cast<char>( regs[3] & 0xFF );
+   m_vendorString[5] = static_cast<char>( ( regs[3] >> 8 ) & 0xFF );
+   m_vendorString[6] = static_cast<char>( ( regs[3] >> 16 ) & 0xFF );
+   m_vendorString[7] = static_cast<char>( ( regs[3] >> 24 ) & 0xFF );
 
-   m_vendorString[8] = static_cast<char>( ecx & 0xFF );
-   m_vendorString[9] = static_cast<char>( ( ecx >> 8 ) & 0xFF );
-   m_vendorString[10] = static_cast<char>( ( ecx >> 16 ) & 0xFF );
-   m_vendorString[11] = static_cast<char>( ( ecx >> 24 ) & 0xFF );
+   m_vendorString[8] = static_cast<char>( regs[2] & 0xFF );
+   m_vendorString[9] = static_cast<char>( ( regs[2] >> 8 ) & 0xFF );
+   m_vendorString[10] = static_cast<char>( ( regs[2] >> 16 ) & 0xFF );
+   m_vendorString[11] = static_cast<char>( ( regs[2] >> 24 ) & 0xFF );
    m_vendorString[12] = '\0';
 
-   // Get information from the registers (Processor info and feature bits)
-   eax = 1;
-   native_cpuid( &eax, &ebx, &ecx, &edx );
+   // Get information from the registers (eax = 1, Processor info and feature bits)
+   native_cpuid( regs, 1 );
 
    // Get information from the registers
-   // stepping   eax, bits 0-3
-   // model      eax, bits 4-7
-   // family     eax, bits 8-11
+   // stepping   regs[0], bits 0-3
+   // model      regs[0], bits 4-7
+   // family     regs[0], bits 8-11
 
-   // Sse        edx, bit 25
-   // Sse2       edx, bit 26
-   // Sse3       ecx, bit 0
-   // Sse42      ecx, bit 20
-   // Avx        ecx, bit 28
-   // Fma3       ecx, bit 12
+   // Sse        regs[3], bit 25
+   // Sse2       regs[3], bit 26
+   // Sse3       regs[2], bit 0
+   // Sse42      regs[2], bit 20
+   // Avx        regs[2], bit 28
+   // Fma3       regs[2], bit 12
 
-   m_family = ( eax >> 8 ) & 0xF;
-   m_model = ( eax >> 4 ) & 0xF;
-   m_stepping = (eax)& 0xF;
+   m_family = ( regs[0] >> 8 ) & 0xF;
+   m_model = ( regs[0] >> 4 ) & 0xF;
+   m_stepping = ( regs[0] ) & 0xF;
 
-   m_supportSse = getBit( edx, 25 );
-   m_supportSse2 = getBit( edx, 26 );
-   m_supportSse3 = getBit( ecx, 0 );
-   m_supportSse42 = getBit( ecx, 20 );
-   m_supportAvx = getBit( ecx, 28 );
-   m_supportFma3 = getBit( ecx, 12 );
+   m_supportSse = getBit( regs[3], 25 );
+   m_supportSse2 = getBit( regs[3], 26 );
+   m_supportSse3 = getBit( regs[2], 0 );
+   m_supportSse42 = getBit( regs[2], 20 );
+   m_supportAvx = getBit( regs[2], 28 );
+   m_supportFma3 = getBit( regs[2], 12 );
 
-   // Get information from the registers (Extended features)
-   eax = 7;
-   ecx = 0;
-   native_cpuid( &eax, &ebx, &ecx, &edx );
+   // Get information from the registers (eax = 7, Extended features)
+   native_cpuid( regs, 7 );
 
    // Get information from the registers
-   // avx2       ebx, bit 5
-   // avx512f    ebx, bit 16
-   // avx512ifma ebx, bit 21
-   // avx512er   ebx, bit 27
-   // avx512er   ebx, bit 27
+   // avx2       regs[1], bit 5
+   // avx512f    regs[1], bit 16
+   // avx512ifma regs[1], bit 21
+   // avx512er   regs[1], bit 27
+   // avx512er   regs[1], bit 27
 
-   m_supportAvx2 = getBit( ebx, 5 );
-   m_supportAvx512 = getBit( ebx, 16 );
-   m_supportAvx512Ifma = getBit( ebx, 21 );
-   m_supportAvx512er = getBit( ebx, 27 );
-
-#endif
+   m_supportAvx2 = getBit( regs[1], 5 );
+   m_supportAvx512 = getBit( regs[1], 16 );
+   m_supportAvx512Ifma = getBit( regs[1], 21 );
+   m_supportAvx512er = getBit( regs[1], 27 );
 }
 
 int cpuInfo::getBit( unsigned int num, int position )
@@ -153,17 +147,19 @@ int cpuInfo::getBit( unsigned int num, int position )
       return 0; // false if the position is out of bounds
 }
 
-#if not defined(_WIN32) && not defined (_WIN64)
-void cpuInfo::native_cpuid( unsigned int *eax, unsigned int *ebx,
-   unsigned int *ecx, unsigned int *edx )
+void cpuInfo::native_cpuid( uint32_t * regs, unsigned eax )
 {
+#if defined(_WIN32) || defined (_WIN64)
+   __cpuid( (int *)regs, (int)eax );
+#else
+   // call assembly instruction cpuid
    asm volatile( "cpuid"
-      : "=a" ( *eax ),
-      "=b" ( *ebx ),
-      "=c" ( *ecx ),
-      "=d" ( *edx )
-      : "0" ( *eax ), "2" ( *ecx ) );
-}
+      : "=a" ( regs[0] ),
+      "=b" ( regs[1] ),
+      "=c" ( regs[2] ),
+      "=d" ( regs[3] )
+      : "a" (eax), "c" (0) );
 #endif
+}
 
 #endif
