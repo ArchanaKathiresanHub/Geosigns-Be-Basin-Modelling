c489e9d889b077d1542578c76144ed8210e09592
Sergey Koshelev <sergey.koshelev@pds.nl>
2017-07-26 11:48:02 +0200

    Add VS2015 debug runtime libraries to be copied to destination together with Swig wrapped C++ libraries

--- development/cmake/CSharpSwigAPITemplate.targets.cmake
+++ development/cmake/CSharpSwigAPITemplate.targets.cmake
@@ -2,11 +2,12 @@
 <Project ToolsVersion="4.0" xmlns="http://schemas.microsoft.com/developer/msbuild/2003"> 
     <ItemGroup Condition=" '$(Configuration)' == '' Or '$(Configuration)' == 'Release' ">
       <ReferenceCopyLocalPaths Include="$(MSBuildThisFileDirectory)..\lib\native\x64\Release\@CSPROJ_NAME@.dll" />
-    </ItemGroup> 
+    </ItemGroup>
     <ItemGroup Condition=" '$(Configuration)' == 'Debug' ">
       <ReferenceCopyLocalPaths Include="$(MSBuildThisFileDirectory)..\lib\native\x64\Debug\@CSPROJ_NAME@.dll" />
       <ReferenceCopyLocalPaths Include="$(MSBuildThisFileDirectory)..\lib\native\x64\Debug\@CSPROJ_NAME@.pdb" />
       <ReferenceCopyLocalPaths Include="$(MSBuildThisFileDirectory)..\lib\net452\Debug\@CSPROJ_NAMESPACE@.@CSPROJ_NAME@.pdb" />
-    </ItemGroup> 
+      <ReferenceCopyLocalPaths Include="$(MSBuildThisFileDirectory)..\lib\native\x64\Debug\msvcp140d.dll" />
+      <ReferenceCopyLocalPaths Include="$(MSBuildThisFileDirectory)..\lib\native\x64\Debug\vcruntime140d.dll" />
+    </ItemGroup>
  </Project>
-   
