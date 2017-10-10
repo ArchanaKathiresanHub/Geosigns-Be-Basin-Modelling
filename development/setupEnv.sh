dfb0662359ca336f69518099de9f992ca83c7491
Massimiliano Beccaria <massimiliano.beccaria@pds.nl>
2017-10-10 14:43:49 +0000

    Merged PR 3137: Added setup script to run RT on Shell
    Added setup script to run RT on Shell.
    Please double-check also the changes in *.ini files in /apps/sssdev/ibs/

---
+++ development/setupEnv.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+# This script is meant to set up the environment in Shell for regression tests.
+# It will be sourced by *.ini files.
+
+intelCmplModuleName="intel/2017.02"
+
+export LD_PRELOAD="/glb/data/hpcrnd/easybuild/PRODUCTION/software/rhel/6/GCCcore/4.9.3/lib64/libstdc++.so"
+loadedMdl=`module list 2>&1 | grep ${intelCmplModuleName}`
+if [ "x${loadedMdl}" == "x" ]; then
+   availMdl=`module av 2>&1 | grep ${intelCmplModuleName}`
+   if [ "x${availMdl}" == "x" ]; then
+      [[ -r /glb/data/hpcrnd/easybuild/public/etc/profile.d/shell-envmodules.sh ]] && . /glb/data/hpcrnd/easybuild/public/etc/profile.d/shell-envmodules.sh
+   fi
+   module purge
+   module load ${intelCmplModuleName}
+fi
+loadedMdl=`module list 2>&1 | grep ${intelCmplModuleName}`
+if [ "x${loadedMdl}" == "x" ]; then
+   echo "Fail to load module ${intelCmplModuleName}. Exiting..."
+   exit 1
+fi
