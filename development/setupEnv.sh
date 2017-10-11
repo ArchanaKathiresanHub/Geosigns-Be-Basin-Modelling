2ccfc91ff9f8932c927ada87676c61dc52fad8b8
Massimiliano Beccaria <massimiliano.beccaria@pds.nl>
2017-10-11 11:50:01 +0200

    removed set -e from development/setupEnv.sh

--- development/setupEnv.sh
+++ development/setupEnv.sh
@@ -1,5 +1,4 @@
 #!/bin/ksh
-set -e
 
 # This script is meant to set up the environment in Shell for regression tests.
 # It will be sourced by *.ini files.
@@ -7,16 +6,16 @@ set -e
 intelCmplModuleName="intel/2017.02"
 
 export LD_PRELOAD="/glb/data/hpcrnd/easybuild/PRODUCTION/software/rhel/6/GCCcore/4.9.3/lib64/libstdc++.so"
-loadedMdl=`module list 2>&1 | grep ${intelCmplModuleName}`
+loadedMdl=$(module list 2>&1 | grep ${intelCmplModuleName})
 if [ "x${loadedMdl}" == "x" ]; then
-   availMdl=`module av 2>&1 | grep ${intelCmplModuleName}`
+   availMdl=$(module av 2>&1 | grep ${intelCmplModuleName})
    if [ "x${availMdl}" == "x" ]; then
       [[ -r /glb/data/hpcrnd/easybuild/public/etc/profile.d/shell-envmodules.sh ]] && . /glb/data/hpcrnd/easybuild/public/etc/profile.d/shell-envmodules.sh
    fi
    module purge
    module load ${intelCmplModuleName}
 fi
-loadedMdl=`module list 2>&1 | grep ${intelCmplModuleName}`
+loadedMdl=$(module list 2>&1 | grep ${intelCmplModuleName})
 if [ "x${loadedMdl}" == "x" ]; then
    echo "Fail to load module ${intelCmplModuleName}. Exiting..."
    exit 1
