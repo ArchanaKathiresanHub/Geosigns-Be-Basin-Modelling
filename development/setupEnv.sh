96291b0b922e5f157a03caf42fc93498f20c9d9f
K.Muthumanickam <K.Muthumanickam@shell.com>
2019-04-05 16:09:47 +0000

    Updated setupEnv.sh to fix RT issues in RHEL 7.6

--- development/setupEnv.sh
+++ development/setupEnv.sh
@@ -10,7 +10,14 @@ loadedMdl=$(module list 2>&1 | grep ${intelCmplModuleName})
 if [ "x${loadedMdl}" == "x" ]; then
    availMdl=$(module av 2>&1 | grep ${intelCmplModuleName})
    if [ "x${availMdl}" == "x" ]; then
-      [[ -r /glb/data/hpcrnd/easybuild/public/etc/profile.d/shell-envmodules.sh ]] && . /glb/data/hpcrnd/easybuild/public/etc/profile.d/shell-envmodules.sh
+      #[[ -r /glb/data/hpcrnd/easybuild/public/etc/profile.d/shell-envmodules.sh ]] && . /glb/data/hpcrnd/easybuild/public/etc/profile.d/shell-envmodules.sh
+      OSTYPE=$(cat /etc/redhat-release | awk '{print $7}')
+      if [[ $OSTYPE == "6.8" || $OSTYPE == "6.9" ]]; then
+          [[ -r /glb/data/hpcrnd/easybuild/public/etc/profile.d/shell-envmodules.sh ]] && . /glb/data/hpcrnd/easybuild/public/etc/profile.d/shell-envmodules.sh
+      else
+          [[ -r /glb/apps/hpc/Lmod/etc/profile.d/z01_lmod-hpcs.sh ]] && . /glb/apps/hpc/Lmod/etc/profile.d/z01_lmod-hpcs.sh -d HpcSofwareStack/PRODUCTION
+          module load HpcSofwareStack/PRODUCTION
+      fi
    fi
    module purge
    module load ${intelCmplModuleName}
