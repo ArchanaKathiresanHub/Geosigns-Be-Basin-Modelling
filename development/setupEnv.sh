70af5ffae377575124f25b4f381ce61a5f4649f2
arijit.khan Khan <arijit.khan@shell.com>
2019-10-04 09:16:58 +0000

    Updated setupEnv.sh to correct the default -d option Spelling correction: HpcSoftwareStack/PRODUCTION which was HpcSotfwareStack/PRODUCTION

--- development/setupEnv.sh
+++ development/setupEnv.sh
@@ -15,7 +15,7 @@ if [ "x${loadedMdl}" == "x" ]; then
       if [[ $OSTYPE == "6.8" || $OSTYPE == "6.9" ]]; then
           [[ -r /glb/data/hpcrnd/easybuild/public/etc/profile.d/shell-envmodules.sh ]] && . /glb/data/hpcrnd/easybuild/public/etc/profile.d/shell-envmodules.sh
       else
-          [[ -r /glb/apps/hpc/Lmod/etc/profile.d/z01_lmod-hpcs.sh ]] && . /glb/apps/hpc/Lmod/etc/profile.d/z01_lmod-hpcs.sh -d HpcSotfwareStack/PRODUCTION
+          [[ -r /glb/apps/hpc/Lmod/etc/profile.d/z01_lmod-hpcs.sh ]] && . /glb/apps/hpc/Lmod/etc/profile.d/z01_lmod-hpcs.sh -d HpcSoftwareStack/PRODUCTION
           module load HpcSoftwareStack/PRODUCTION
       fi
    fi
