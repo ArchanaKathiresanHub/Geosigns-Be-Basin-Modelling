d6e90239145b8c96fd77be6d3b51df4be0c7b957
faraz.khatami <faraz.khatami@shell.com>
2020-02-11 11:15:59 +0100

    #336255 name mapping of type I_II cenozoic with sulphur included

--- development/libraries/genex6_kernel/src/SourceRockTypeNameMappings.cpp
+++ development/libraries/genex6_kernel/src/SourceRockTypeNameMappings.cpp
@@ -57,7 +57,8 @@ void SourceRockTypeNameMappings::setPropertyMaps()
   typeNamesSR = {"Type_I_II_Mesozoic_MarineShale_lit",
                  "Type_I_II_Mesozoic_Marine_Shale_lit"};
   typeNamesSRWithSulphur = {"Type_I_II_Mesozoic_MarineShale_lit_s",
-                           "Type_I_II_Mesozoic_Marine_Shale_lit_s"};
+                           "Type_I_II_Mesozoic_Marine_Shale_lit_s",
+                           "Type_I_II_Cenozoic_Marine_Marl_lit_s"};
   m_typeNamesSR["TypeI_II"] = typeNamesSR;
   m_typeNamesSRWithSulphur["TypeI_II"] = typeNamesSRWithSulphur;
 
