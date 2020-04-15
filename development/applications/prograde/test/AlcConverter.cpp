75f87827939cef9281fa14798e736d5b6fa237e1
sumant.kalra <sumantkalra109@gmail.com>
2020-04-15 17:25:15 +0530

    resolving conflicts_1

--- development/applications/prograde/test/AlcConverter.cpp
+++ development/applications/prograde/test/AlcConverter.cpp
@@ -25,16 +25,15 @@ TEST(AlcModelConverter, upgrade)
 	std::string originalALC = "Advanced Lithosphere Calculator";
 	std::string text = "Anything_xyz";
 
-	EXPECT_EQ(BottomBoundaryManager::StandardCondModel, modelConverter.upgradeAlcCrustPropModel(BottomBoundaryManager::LegacyCrust));
-	EXPECT_EQ(BottomBoundaryManager::StandardCondModel, modelConverter.upgradeAlcCrustPropModel(BottomBoundaryManager::LowCondModel));
-	EXPECT_EQ(BottomBoundaryManager::StandardCondModel, modelConverter.upgradeAlcCrustPropModel(BottomBoundaryManager::StandardCondModel));
+	EXPECT_EQ(BottomBoundaryManager::CrustPropertyModel::StandardCondModel, modelConverter.upgradeAlcCrustPropModel(BottomBoundaryManager::CrustPropertyModel::LegacyCrust));
+	EXPECT_EQ(BottomBoundaryManager::CrustPropertyModel::StandardCondModel, modelConverter.upgradeAlcCrustPropModel(BottomBoundaryManager::CrustPropertyModel::LowCondModel));
+	EXPECT_EQ(BottomBoundaryManager::CrustPropertyModel::StandardCondModel, modelConverter.upgradeAlcCrustPropModel(BottomBoundaryManager::CrustPropertyModel::StandardCondModel));
 
-	EXPECT_EQ(BottomBoundaryManager::HighCondMnModel, modelConverter.upgradeAlcMantlePropModel(BottomBoundaryManager::LegacyMantle));
-	EXPECT_EQ(BottomBoundaryManager::HighCondMnModel, modelConverter.upgradeAlcMantlePropModel(BottomBoundaryManager::LowCondMnModel));
-	EXPECT_EQ(BottomBoundaryManager::HighCondMnModel, modelConverter.upgradeAlcMantlePropModel(BottomBoundaryManager::StandardCondMnModel));
-	EXPECT_EQ(BottomBoundaryManager::HighCondMnModel, modelConverter.upgradeAlcMantlePropModel(BottomBoundaryManager::HighCondMnModel));
+	EXPECT_EQ(BottomBoundaryManager::MantlePropertyModel::HighCondMnModel, modelConverter.upgradeAlcMantlePropModel(BottomBoundaryManager::MantlePropertyModel::LegacyMantle));
+	EXPECT_EQ(BottomBoundaryManager::MantlePropertyModel::HighCondMnModel, modelConverter.upgradeAlcMantlePropModel(BottomBoundaryManager::MantlePropertyModel::LowCondMnModel));
+	EXPECT_EQ(BottomBoundaryManager::MantlePropertyModel::HighCondMnModel, modelConverter.upgradeAlcMantlePropModel(BottomBoundaryManager::MantlePropertyModel::StandardCondMnModel));
+	EXPECT_EQ(BottomBoundaryManager::MantlePropertyModel::HighCondMnModel, modelConverter.upgradeAlcMantlePropModel(BottomBoundaryManager::MantlePropertyModel::HighCondMnModel));
 
 	EXPECT_EQ("Improved Lithosphere Calculator Linear Element Mode", modelConverter.updateBottomBoundaryModel(originalALC));
 	EXPECT_EQ(text, modelConverter.updateBottomBoundaryModel(text));
-	
 }
