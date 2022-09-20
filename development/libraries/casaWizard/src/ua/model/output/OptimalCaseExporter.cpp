b0411e40266c52529841b3dd18299dbbbb0d3f6b
Sven van Leeuwen <Sven.van-Leeuwen@shell.com>
2022-09-20 12:14:04 +0000

    chg: usr: Edit scenarioname in UA
    * Changed the scenario name when exporting the optimalcase from the UA wizard to:  originalscenarioname_UA_date-time
    ================== COMMIT-END ========================

--- development/libraries/casaWizard/src/ua/model/output/OptimalCaseExporter.cpp
+++ development/libraries/casaWizard/src/ua/model/output/OptimalCaseExporter.cpp
@@ -9,10 +9,12 @@
 #include "OptimalCaseExporter.h"
 
 #include "model/functions/folderOperations.h"
+#include "model/output/workspaceGenerator.h"
 #include "model/logger.h"
 
 #include <QDir>
 #include <QFile>
+#include <QTextStream>
 
 namespace casaWizard
 {
@@ -56,8 +58,8 @@ void exportOptimalCase(const QString& optimalCaseDirectory, const QString& worki
 {
    if (!checkExists(optimalCaseDirectory + "/Project.project3d")) return;
 
-   if (!tryCopy(workingDirectory,optimalCaseDirectory,"Project.txt")) return;
-   if (!tryCopy(workingDirectory,optimalCaseDirectory,"Inputs.HDF")) return;
+   if (!tryCopy(workingDirectory, optimalCaseDirectory, "Project.txt")) return;
+   if (!tryCopy(workingDirectory, optimalCaseDirectory, "Inputs.HDF")) return;
 
    QDir tmpdir(optimalCaseDirectory + "/tmpDirectory");
    tmpdir.removeRecursively();
@@ -76,6 +78,40 @@ void exportOptimalCase(const QString& optimalCaseDirectory, const QString& worki
       return;
    }
 
+   QFile project(optimalCaseDirectory + "Project.txt");
+   if(!project.open(QIODevice::ReadWrite | QFile::Text)) {
+
+   }
+
+   QTextStream inOut(&project);
+   QString text;
+   QString scenarioTitle;
+
+   while(!inOut.atEnd()) {
+      QString line = inOut.readLine();
+      QStringList fields = line.split(":");
+      if(fields.size() >= 2 && fields.first() == "Scenario")
+      {
+         scenarioTitle = fields.last().simplified();
+         //check if the scenario didnt already have the tags added
+         if (!scenarioTitle.contains("_UA_"))
+         {
+            if (workingDirectory.contains("/casaWorkspace-"))
+            {
+               scenarioTitle = scenarioTitle + "_UA_" + workingDirectory.split("/casaWorkspace-").last().split("/").first();
+            }
+            else
+            {
+               scenarioTitle = workspaceGenerator::getTimeStamp(scenarioTitle + "_UA_");
+            }
+            line = fields.first() + ": " + scenarioTitle;
+         }
+      }
+      text.push_back(line + "\n");
+   }
+   inOut << text;
+
+   project.close();
    functions::zipFolderContent(tmpdir, optimalCaseDirectory, "optimal");
    tmpdir.removeRecursively();
 }
